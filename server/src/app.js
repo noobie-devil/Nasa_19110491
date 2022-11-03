66c77c2ca7432362fa10036478644ed76cbb25b3
truongnguyenvan8801 <school0123456@gmail.com>
2022-11-03 22:51:56 +0700

    commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,23 @@
+const path = require('path');
 const express = require('express');
-
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
-
+const launchesRouters = require('./routes/launches/launches.router');
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
 app.use(express.json());
-app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouters);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
